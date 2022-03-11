b6df570e5bda74ffc3031b660a22fcb7ee37b861
Yakooboo <88765908+Yakooboo@users.noreply.github.com>
2022-03-11 12:50:20 +0100

    commit

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://a3e5e97a.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
